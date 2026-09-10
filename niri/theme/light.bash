55fac4f99105382e7a048ab08456ecfd27015ac7
twodogsdave <43505645+twodogsdave@users.noreply.github.com>
2026-09-10 17:56:19 -0400

    Add files via upload

---
+++ niri/theme/light.bash
@@ -0,0 +1,37 @@
+# ------------------------------------------------------------------------------
+# Copyright (C) 2020-2026 Aditya Shakya <adi1090x@gmail.com>
+#
+# Light Theme
+# ------------------------------------------------------------------------------
+
+# Colors
+background='#ffffff'
+foreground='#383a42'
+color0='#383a42'
+color1='#e45649'
+color2='#50a14f'
+color3='#c18401'
+color4='#0184bc'
+color5='#a626a4'
+color6='#0997b3'
+color7='#909090'
+color8='#383a42'
+color9='#e45649'
+color10='#50a14f'
+color11='#c18401'
+color12='#0184bc'
+color13='#a626a4'
+color14='#0997b3'
+color15='#909090'
+
+# Wallpaper
+wallpaper="$HOME/.config/niri/wallpapers/light.jpg"
+
+# GTK Theme
+gtk_theme='White'
+gtk_icons='Luv-Folders'
+gtk_font='Noto Sans 9'
+cursor_theme='Qogirr-Dark'
+
+# Geany
+geany_colors='slime.conf'
